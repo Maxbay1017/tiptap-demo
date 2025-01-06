5e5633360826c5c4bf2990b6a6cfbfd88685a73a
Collin Liu <junnebailiu@gmail.com>
2025-01-06 19:27:34 +0800

    tiptap

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto max-w-4xl my-8">
+    <EditorContent :editor="editor" />
+  </div>
+</template>
+
+<script setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  editorProps: {
+    attributes: {
+      class: 'border border-gray-400 p-4 mx-auto focus:outline-none min-h-[12rem]',
+    },
+  },
+  content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
+  extensions: [StarterKit],
+})
+</script>
